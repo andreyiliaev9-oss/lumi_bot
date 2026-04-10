9077b58d5795794b7e56f71b164d64a01f533c9b
andreyiliaev9-oss <andreyiliaev9@gmail.com>
2026-04-10 13:54:22 +0500

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,12 +1,2 @@
-        # Таблица для планировщика
-        await db.execute("""
-            CREATE TABLE IF NOT EXISTS tasks (
-                task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER,
-                title TEXT,
-                task_time TIMESTAMP,
-                remind_type TEXT,
-                is_notified INTEGER DEFAULT 0
-            )
-        """)
-
+from .database import 
+init_db, DB_NAME
