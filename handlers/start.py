05f5e865082433647dc88a444d654e0ef70b240b
andreyiliaev9-oss <andreyiliaev9@gmail.com>
2026-04-11 18:04:16 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,4 +1,4 @@
- from aiogram import Router, F
+from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
